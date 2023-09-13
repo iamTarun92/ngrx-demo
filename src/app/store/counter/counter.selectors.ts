3537450e221224308a2632cb32bc20a6c918dff7
iamTarun92 <69888235+iamTarun92@users.noreply.github.com>
2023-09-13 18:14:19 +0530

    minor fix

--- src/app/store/counter/counter.selectors.ts
+++ src/app/store/counter/counter.selectors.ts
@@ -1,12 +1,6 @@
 import { createSelector, createFeatureSelector } from '@ngrx/store';
 import { counterState } from 'src/app/models/user';
 
-export const featureKey = 'feature';
-
-export interface FeatureState {
-    counter: number;
-}
-
 export const getCounterState = createFeatureSelector<counterState>('counter');
 
 export const getCounter = createSelector(
